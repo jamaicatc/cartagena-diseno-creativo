c2cb3de0b255177a8f65a0f7bc17111463047f46
jamaicatc <164107464+jamaicatc@users.noreply.github.com>
2024-05-26 04:23:26 -0500

    Delete version 2 del portafolio/portafolio directory
    se borra, porque no se trajeron correctamente los archivos a github

--- version 2 del portafolio/portafolio/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.getElementById('bars');
-    const navLinks = document.getElementById('nav-links');
-
-    burger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
